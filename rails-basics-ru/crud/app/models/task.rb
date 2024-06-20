155af6c1bf6abb1c682fb0ba98a76f3da56b03c8
Arina Zaitseva <avanero@bk.ru>
2024-06-19 22:03:44 -0400

    create model Task, routes, fixtures etc

---
+++ rails-basics-ru/crud/app/models/task.rb
@@ -0,0 +1,6 @@
+class Task < ApplicationRecord
+  validates :name, :status, :creator, :completed, presence: true
+
+  # attribute :status, :string, default: -> { 'new' }
+  # attribute :completed, :string, default: -> { false }
+end
--- rails-basics-ru/crud/config/routes.rb
+++ rails-basics-ru/crud/config/routes.rb
@@ -2,8 +2,6 @@
 
 Rails.application.routes.draw do
   root 'home#index'
-  # BEGIN
-  
-  # END
+  resources :tasks, only: %i[show new create edit update destroy]
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
