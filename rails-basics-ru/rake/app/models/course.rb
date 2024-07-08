2ef27c905a7055c13e37ca6f6de97737bb10f6ff
Arina Zaitseva <avanero@bk.ru>
2024-07-08 15:52:22 -0400

    Create many-to-many association btw users&courses

--- rails-basics-ru/rake/app/models/course.rb
+++ rails-basics-ru/rake/app/models/course.rb
@@ -1,3 +1,5 @@
 class Course < ApplicationRecord
   has_many :lessons
+  has_many :courses_users
+  has_many :users, through: :courses_users
 end
--- rails-basics-ru/rake/app/models/user.rb
+++ rails-basics-ru/rake/app/models/user.rb
@@ -2,6 +2,8 @@
 
 class User < ApplicationRecord
   has_many :posts, foreign_key: 'creator_id'
+  has_many :courses_users
+  has_many :courses, through: :courses_users
 
   validates :birthday, :first_name, :last_name, :email, presence: true
 end
