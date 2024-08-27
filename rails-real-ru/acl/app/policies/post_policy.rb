6d6fa0e5f04da49b0c3265d7e46bc1abc3d8dc95
Arina Zaitseva <avanero@bk.ru>
2024-08-27 17:48:06 -0400

    use pundit for authorisation

--- rails-real-ru/acl/app/policies/post_policy.rb
+++ rails-real-ru/acl/app/policies/post_policy.rb
@@ -1,7 +1,23 @@
 # frozen_string_literal: true
 
 class PostPolicy < ApplicationPolicy
-  # BEGIN
-  
-  # END
+  def new?
+    user.present?
+  end
+
+  def create?
+    new?
+  end
+
+  def edit?
+    user&.admin? || user&.id == record.author.id
+  end
+
+  def update?
+    edit?
+  end
+
+  def destroy?
+    user&.admin?
+  end
 end
