da39b1af3be7df3ce16a8ed071e49b9d8b272449
Arina Zaitseva <avanero@bk.ru>
2024-09-04 20:54:40 -0400

    Add an image to vehicle

--- rails-real-ru/files/app/models/vehicle.rb
+++ rails-real-ru/files/app/models/vehicle.rb
@@ -1,9 +1,11 @@
 # frozen_string_literal: true
 
 class Vehicle < ApplicationRecord
-  # BEGIN
-  
-  # END
+  has_one_attached :image do |attachable|
+    attachable.variant :thumb, resize_to_limit: [200, 100]
+  end
+
+  MAX_IMAGE_SIZE_MB = 5
 
   validates :manufacture, presence: true
   validates :model, presence: true
@@ -12,7 +14,8 @@ class Vehicle < ApplicationRecord
   validates :kilometrage, presence: true, numericality: { only_integer: true }
   validates :production_year, presence: true
 
-  # BEGIN
-  
-  # END
+  validates :image,
+            attached: true,
+            size: { less_than: MAX_IMAGE_SIZE_MB.megabytes },
+            content_type: ['image/png', 'image/jpg', 'image/jpeg']
 end
