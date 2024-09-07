99d01312c87ce1bec2a58c6402c40ab799e0d2da
@hexlet/cli <support@hexlet.io>
2024-09-07 19:36:12 -0400

    submit rails-full-ru/jbuilder

--- rails-full-ru/jbuilder/app/controllers/api/v2/users_controller.rb
+++ rails-full-ru/jbuilder/app/controllers/api/v2/users_controller.rb
@@ -1,7 +1,13 @@
 # frozen_string_literal: true
 
 class Api::V2::UsersController < Api::ApplicationController
-  # BEGIN
+  def index
+    @users = User.order(created_at: :desc)
+    render json: @users, each_serializer: UserSerializer
+  end
 
-  # END
+  def show
+    @user = User.find(params[:id])
+    render json: @user
+  end
 end
