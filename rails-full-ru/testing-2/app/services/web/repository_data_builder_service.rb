3921c83bc7a323a57f5ce8fdac3c64d1b3825ea9
Arina Zaitseva <avanero@bk.ru>
2024-09-10 16:26:43 -0400

    test service with stubbing a request

--- rails-full-ru/testing-2/app/services/web/repository_data_builder_service.rb
+++ rails-full-ru/testing-2/app/services/web/repository_data_builder_service.rb
@@ -1,3 +1,5 @@
+# frozen_string_literal: true
+
 class Web::RepositoryDataBuilderService
   def build(link)
     repository_data = retrieve_repository_data(link)
@@ -9,20 +11,23 @@ class Web::RepositoryDataBuilderService
   def retrieve_repository_data(link)
     client = create_client
     octokit_repo = Octokit::Repository.from_url(link)
-    client.repository(octokit_repo)
+    response = client.repository(octokit_repo)
+    return JSON.parse(response) if response.is_a?(String)
+
+    response
   end
 
   def build_params(repository_data)
     {
-      link: repository_data[:url],
-      owner_name: repository_data[:owner][:login],
-      repo_name: repository_data[:name], # full_name
-      description: repository_data[:description],
-      default_branch: repository_data[:default_branch],
-      watchers_count: repository_data[:watchers_count],
-      language: repository_data[:language],
-      repo_created_at: repository_data[:created_at],
-      repo_updated_at: repository_data[:updated_at]
+      link: repository_data['url'],
+      owner_name: repository_data['owner']['login'],
+      repo_name: repository_data['name'],
+      description: repository_data['description'],
+      default_branch: repository_data['default_branch'],
+      watchers_count: repository_data['watchers_count'],
+      language: repository_data['language'],
+      repo_created_at: repository_data['created_at'],
+      repo_updated_at: repository_data['updated_at']
     }
   end
 
