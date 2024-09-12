270fb6c46da25d501a43a3245b2401ce9e0cda05
Arina Zaitseva <avanero@bk.ru>
2024-09-12 00:02:49 -0400

    minor fixes

--- rails-full-ru/caching/app/models/article.rb
+++ rails-full-ru/caching/app/models/article.rb
@@ -3,7 +3,7 @@
 class Article < ApplicationRecord
   def last_reading_date
     Rails.cache.fetch("#{cache_key_with_version}/last_reading_date", expires_in: 12.hours) do
-      Time.now
+      DateTime.now
     end
   end
 end
